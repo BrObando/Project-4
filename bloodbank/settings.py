d06a0f75e9e088013a8555e720860b98ce77dc88
BrObando <brianna.obando@gmail.com>
2024-01-21 15:45:43 -0500

    bugs

--- bloodbank/settings.py
+++ bloodbank/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 
 import os
 
-from dotenv import load_dotenv
+
 
 # Load environment variables from .env
 load_dotenv(os.path.join(Path(__file__).resolve().parent, '.env'))
